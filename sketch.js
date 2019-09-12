4a93459bda4a71b902ac9f7f7c8ee0d25aab5631
Jason <shahiemsantos@aol.com>
2019-09-12 15:35:06 -0400

    protrait

--- sketch.js
+++ sketch.js
@@ -4,5 +4,20 @@ function setup() {
 
 function draw()  {
     background("yellow");
-    ellipse(320,180,100);
+    fill("#CC9E52")
+    noStroke();
+    ellipse(320,200,300,300); // face
+    
+    
+    stroke("Black");
+    fill(0,125)
+    strokeWeight(5);
+     ellipse(235,175,40,50)
+      ellipse(395,175,40,50)
+    rect(190,150,100,60); // left lens
+    rect(350,150,100,60); // right lens
+    
+    
+    fill("White")
+    arc(320,300,80,80,0,PI);
 }
