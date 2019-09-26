a0558d2af410f0d1a7028ad74b8285effa45caf6
Jason <shahiemsantos@aol.com>
2019-09-26 15:39:19 -0400

    Update interactiveselfportrait.js

--- interactive/interactiveselfportrait.js
+++ interactive/interactiveselfportrait.js
@@ -1,17 +1,27 @@
 /* self porteait with variables */
 
-var x = 320
-var y = 200
+var x = 320;
+var y = 200;
 var facesize = 400;
-    
+
 function setup() {
-    createCanvas(640,360,WEBGL);
+    createCanvas(640, 360, WEBGL);
+    normalMaterial();
+    cam = createCamera();
+    rectMode(CENTER);
 }
 
-function draw()  {
-  background(200);
-    fill("red");
-    rotateY(mouseX/1000);
-    
-  sphere(100);
+function draw() {
+    background(300);
+    var r = 0
+    var g = mouseX
+    var b = mouseY
+    fill(r, g, b)
+    rotateY(mouseX / 100);
+    fill("green")
+    sphere(100, 200, 200);
+
+    fill("red")
+    sphere(100);
+
 }
