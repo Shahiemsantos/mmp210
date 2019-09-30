37d3c36f0308474919ee793a30a11936b4ae09b2
Shahiemsantos <Shahiemsantos@aol.com>
2019-09-29 21:21:32 -0400

    Update interactiveselfportrait.js

--- interactive/interactiveselfportrait.js
+++ interactive/interactiveselfportrait.js
@@ -6,9 +6,6 @@ var facesize = 400;
 
 function setup() {
     createCanvas(640, 360, WEBGL);
-    normalMaterial();
-    cam = createCamera();
-    rectMode(CENTER);
 }
 
 function draw() {
@@ -17,24 +14,24 @@ function draw() {
     var g = mouseX
     var b = mouseY
     fill(r, g, b)
-   
-    rect(30, 20, x - 55 + frameCount * 2, 25);
-    fill(g, r, b)
-    rect(30, 60, x - 55 + frameCount * 2, 25);
-    fill(b, r, g)
-    rect(30, -30, x - 55 + frameCount * 2, 25);
-    fill(r, b, g)
-rect(30, -80, x - 55 + frameCount * 2, 25);
 
 
     fill("red");
-    sphere(100 + frameCount / 5);
+    sphere(100);
 	
+	 fill(r, b, g)
 	 rotateY(mouseX / 100);
-	translate(-200, 200, 100);
-    sphere(10 + frameCount / 5);
-
-
-    cam.move(0, 0, frameCount / 1000);
+	 translate(-150, -50, 100);
+    sphere(50);
+	translate(-100, 0, 100);
+    sphere(50);
+	translate(100, 50, 100);
+    sphere(50);
+    translate(50, 100, 100);
+    sphere(50);
 
 }
+	function mousePressed(){
+
+		
+	}
